98e535599d8e301f4f3000bfae1d954a990ad0d8
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2012-11-29 04:42:31 +0000

    doc update

--- PlantCell Core/src/au/edu/unimelb/plantcell/core/SequenceProcessor.java
+++ PlantCell Core/src/au/edu/unimelb/plantcell/core/SequenceProcessor.java
@@ -8,8 +8,8 @@ public interface SequenceProcessor {
 	 * Called during iteration over the input table, this method must process the
 	 * sequence, perform any cleanup or validation desired and return the result
 	 * ready for the caller (a node implementation). If null is returned the sequence
-	 * will be skipped. The implementation is free to throw an Exception which will be
-	 * given to the {@link Node} implementation
+	 * will be skipped.
+	 *
 	 * @return the sequence to provide to the node. Usually this will be the input sequence
 	 */
 	public SequenceValue process(SequenceValue sv);
