f3216fb1f5bf8680544d42f026fdfb9943a9e901
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-02-15 04:02:59 +0000

    Added stop codon removal and gap removal to sequence processing plugin. Bumped plugin version to v1.2.8

--- BioJava Processor/src/au/edu/unimelb/plantcell/core/biojava/tasks/SequenceCleanerTask.java
+++ BioJava Processor/src/au/edu/unimelb/plantcell/core/biojava/tasks/SequenceCleanerTask.java
@@ -7,7 +7,6 @@ import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
-import org.knime.core.data.DataType;
 import org.knime.core.data.def.IntCell;
 import org.knime.core.node.InvalidSettingsException;
 
@@ -27,6 +26,7 @@ public class SequenceCleanerTask extends BioJavaProcessorTask {
 	private final Set<Character> m_dna = new HashSet<Character>();
 	private final Set<Character> m_rna = new HashSet<Character>();
 	private final Set<Character> m_aa  = new HashSet<Character>();
+	private String m_task;
 	
 	@Override
 	public String getCategory() {
@@ -36,6 +36,8 @@ public class SequenceCleanerTask extends BioJavaProcessorTask {
 	@Override
 	public void init(String task_name, int col) throws Exception {
 		super.init(task_name, col);
+		m_task = task_name;
+		
 		// DNA
 		for (char c : new char[] { 'A', 'C', 'G', 'T' }) {
 			m_dna.add(new Character(c));
@@ -68,19 +70,33 @@ public class SequenceCleanerTask extends BioJavaProcessorTask {
 	}
 
 	public String[] getNames() { 
-		return new String[] {"Clean sequences (whitespace removal, non-coding symbols, stop codon->X conversion, all uppercase)"}; 
+		// NB: new tasks must be appended for backward compatibility
+		return new String[] {"Clean sequences (whitespace removal, non-coding symbols, stop codon->X conversion, all uppercase)",
+				"Gap removal: leading and trailing gaps only",
+				"Gap removal: all gaps",
+				"Stop codon removal: at end only (protein sequence only)"
+		}; 
 	}
 	
 	public String getHTMLDescription(String task) {
-		return "<html>Performs a series of simple tasks to the chosen column of sequences:" +
-				"<ul>" +
-				"<li>Removal of all whitespace (space, tab, other)</li>"+
-				"<li>Conversion of all residue symbols to uppercase</li>"+
-				"<li>Removal of trailing stop codon(s). Conversion of remaining stop codons to X</li>"+
-				"<li>Removal of non-coding symbols for the chosen sequence type. A count " +
-				" of any residues removed from sequences appears in a separate column.</li>"  +
-				"<li>Removal of all graphical annotations</li>"+
-				"</ul>";
+		if (task.startsWith("Clean sequen")) {
+			return "<html>Performs a series of simple tasks to the chosen column of sequences:" +
+					"<ul>" +
+					"<li>Removal of all whitespace (space, tab, other)</li>"+
+					"<li>Conversion of all residue symbols to uppercase</li>"+
+					"<li>Removal of trailing stop codon(s). Conversion of remaining stop codons to X</li>"+
+					"<li>Removal of non-coding symbols for the chosen sequence type. A count " +
+					" of any residues removed from sequences appears in a separate column.</li>"  +
+					"<li>Removal of all graphical annotations</li>"+
+					"</ul>";
+		} else if (task.startsWith("Gap removal: all")) {
+			return "<html>Removes all hyphen characters (-) denoting a gapped region from an aligned sequence. Useful" +
+					" if you want the original sequence.";
+		} else if (task.startsWith("Gap removal")) {
+			return "<html>Removes gap symbols before the first, and after the last, residue. Other gaps are preserved.";
+		} else {
+			return "<html>Remove trailing stop codons from the end of the specified protein sequence";
+		}
 	}
 	
 	@Override
@@ -89,59 +105,92 @@ public class SequenceCleanerTask extends BioJavaProcessorTask {
 		if (sv == null)
 			return missing_cells(getColumnSpecs().length);
 		
-		String seq = sv.getStringValue().toUpperCase();
-		int before_len = seq.length();
-		int rejected = 0;
-		seq = seq.replaceAll("\\*+$", "");			// remove trailing stop codons
-		if (seq.length() != before_len) {
-			rejected += before_len - seq.length();
-			before_len = seq.length();
-		}
-		seq = seq.replaceAll("\\s+", "");
-		if (before_len != seq.length()) {
-			rejected += before_len - seq.length();
-		}
-		// BUG: reject is not altered for these two steps
-		seq = seq.replaceAll("\\*", "X");
-		seq = seq.toUpperCase();
-		
-		SequenceType st = sv.getSequenceType();
-		Set<Character> letters = m_dna;
-		if (st.isDNA()) {
-			letters = m_dna;
-		} else if (st.isRNA()) {
-			letters = m_rna;
-		} else {		// assume protein
-			letters = m_aa;
-		}
-		StringBuffer sb = new StringBuffer(seq.length());
-		for (int i=0; i<seq.length(); i++) {
-			Character c2 = new Character(seq.charAt(i));
-			if (Character.isLetter(c2)) {
-				if (!letters.contains(c2)) {     // AA
-					rejected++;
-				} else {
-					sb.append(c2);
-				}
-			} else if (c2 == '-' || c2 == '.') {		// pass gap characters through unchanged
-				sb.append(c2);
-			} else {									// else... reject and suppress
-				rejected++;
-			}
-		}
-		
 		DataCell[] cells = new DataCell[2];
 		try {
-			SequenceCell sc = new SequenceCell(st, sv.getID(), sb.toString());
-			if (sc.hasDescription()) {
-				sc.addComment(new Comment(sc.getDescription()));
+			if (m_task.startsWith("Clean")) {
+				String seq = sv.getStringValue().toUpperCase();
+				int before_len = seq.length();
+				int rejected = 0;
+				seq = seq.replaceAll("\\*+$", "");			// remove trailing stop codons
+				if (seq.length() != before_len) {
+					rejected += before_len - seq.length();
+					before_len = seq.length();
+				}
+				seq = seq.replaceAll("\\s+", "");
+				if (before_len != seq.length()) {
+					rejected += before_len - seq.length();
+				}
+				// BUG: reject is not altered for these two steps
+				seq = seq.replaceAll("\\*", "X");
+				seq = seq.toUpperCase();
+				
+				SequenceType st = sv.getSequenceType();
+				Set<Character> letters = m_dna;
+				if (st.isDNA()) {
+					letters = m_dna;
+				} else if (st.isRNA()) {
+					letters = m_rna;
+				} else {		// assume protein
+					letters = m_aa;
+				}
+				StringBuffer sb = new StringBuffer(seq.length());
+				for (int i=0; i<seq.length(); i++) {
+					Character c2 = new Character(seq.charAt(i));
+					if (Character.isLetter(c2)) {
+						if (!letters.contains(c2)) {     // AA
+							rejected++;
+						} else {
+							sb.append(c2);
+						}
+					} else if (c2 == '-' || c2 == '.') {		// pass gap characters through unchanged
+						sb.append(c2);
+					} else {									// else... reject and suppress
+						rejected++;
+					}
+				}
+				
+				SequenceCell sc = new SequenceCell(st, sv.getID(), sb.toString());
+				if (sc.hasDescription()) {
+					sc.addComment(new Comment(sc.getDescription()));
+				}
+				cells[0] = sc;
+				cells[1] = new IntCell(rejected);
+				return cells;
+			} else if (m_task.startsWith("Gap removal")) {
+				if (m_task.indexOf("all") >= 0) {
+					String seq = sv.getStringValue();
+					int before = sv.getLength();
+					seq.replaceAll("-", "");
+					int after = seq.length();
+					cells[0] = new SequenceCell(sv.getSequenceType(), sv.getID(), seq);
+					cells[1] = new IntCell(before - after);
+				} else {
+					String seq = sv.getStringValue();
+					int before = sv.getLength();
+					seq.replaceFirst("^\\-+", "");
+					seq.replaceFirst("\\-+$", "");
+					int after = seq.length();
+					cells[0] = new SequenceCell(sv.getSequenceType(), sv.getID(), seq);
+					cells[1] = new IntCell(before - after);
+				}
+				return cells;
+			} else { // if (m_task.startsWith("Stop codon removal")) {
+				if (!sv.getSequenceType().isProtein()) 
+					return missing_cells(2);
+				String seq = sv.getStringValue();
+				if (seq.endsWith("*")) {
+					cells[0] = new SequenceCell(sv.getSequenceType(), sv.getID(), seq.substring(0, seq.length()-1));
+					cells[1] = new IntCell(1);
+					return cells;
+				} else {
+					cells[0] = new SequenceCell(sv.getSequenceType(), sv.getID(), seq);
+					cells[1] = new IntCell(0);
+					return cells;
+				}
 			}
-			cells[0] = sc;
 		} catch (InvalidSettingsException e) {
-			cells[0] = DataType.getMissingCell();
 			e.printStackTrace();
+			return missing_cells(2);
 		}
-		cells[1] = new IntCell(rejected);
-		return cells;
 	}
 }
