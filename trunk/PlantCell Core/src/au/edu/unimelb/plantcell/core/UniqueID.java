ed4f2d08255b97da9834fc9c6163bbb781cabe33
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-04-18 21:47:42 +0000

    Modified UniqueID(String) to throw if an invalid String is given. Callers must now catch or throw this exception. Unlikely to impact user's as this constructor is not under user control. Javadoc updates.

--- trunk/PlantCell Core/src/au/edu/unimelb/plantcell/core/UniqueID.java
+++ trunk/PlantCell Core/src/au/edu/unimelb/plantcell/core/UniqueID.java
@@ -1,10 +1,22 @@
 package au.edu.unimelb.plantcell.core;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.knime.core.node.InvalidSettingsException;
 
 
+/**
+ * Used to map results from external programs to sequences within KNIME. An important class with great care
+ * taken in the definition of the methods here. Change with caution and test BLAST/EMBOSS/... with care!
+ * 
+ * @author andrew.cassin
+ *
+ */
 public class UniqueID {
 	public static int cur = 1;
 	private final int m_id;
+	private static Pattern p = Pattern.compile("^[^\\d]+?(\\d+)");
 	
 	public UniqueID(boolean restart) {
 		if (restart) 
@@ -16,8 +28,19 @@ public class UniqueID {
 		this(false);
 	}
 	
-	public UniqueID(String str) {
-		m_id = Integer.valueOf(str.substring(1));
+	/**
+	 * Takes an id from (eg. EMBOSS results) like "S10_1" and instantiate a UniqueID with
+	 * the id 10. Used to identify which sequence in a batch the reported result relates to. We cant use
+	 * the user-supplied sequence ID's as they may be incompatible with the chosen program. 
+	 * 
+	 * @param numeric_seq_id a string like "S10_1". Must not be null.
+	 * @throws InvalidSettingsException thrown only if the input does not match the required format
+	 */
+	public UniqueID(String numeric_seq_id) throws InvalidSettingsException {
+		Matcher m = p.matcher(numeric_seq_id);
+		if (!m.find())
+			throw new InvalidSettingsException("Expected numeric id of the form: <letters><digit>+<other stuff>!");
+		m_id = Integer.valueOf(m.group(1));
 	}
 
 	private static synchronized void restart() {
