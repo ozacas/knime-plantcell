2ae76ca0eef29b6c0a5d968a92c24fedcca29bee
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-05-16 23:27:38 +0000

    Added support for filename suffixes: can write spectra to multiple files depending on the value of a particular column (which is part of the filename).

--- Proteomics/src/au/edu/unimelb/plantcell/io/write/spectra/SpectraWriterNodeDialog.java
+++ Proteomics/src/au/edu/unimelb/plantcell/io/write/spectra/SpectraWriterNodeDialog.java
@@ -2,18 +2,18 @@ package au.edu.unimelb.plantcell.io.write.spectra;
 
 import javax.swing.JFileChooser;
 
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.IntValue;
+import org.knime.core.data.StringValue;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
-import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
-import org.knime.core.node.defaultnodesettings.DialogComponentStringListSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
-import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.util.ColumnFilter;
 
-import au.edu.unimelb.plantcell.io.read.spectra.AbstractSpectraCell;
 import au.edu.unimelb.plantcell.io.read.spectra.SpectraValue;
 
 /**
@@ -34,13 +34,43 @@ public class SpectraWriterNodeDialog extends DefaultNodeSettingsPane {
      * This is just a suggestion to demonstrate possible default dialog
      * components.
      */
-    protected SpectraWriterNodeDialog() {
+    @SuppressWarnings("unchecked")
+	protected SpectraWriterNodeDialog() {
         super();
         
-        addDialogComponent(new DialogComponentFileChooser(new SettingsModelString(SpectraWriterNodeModel.CFGKEY_FILE, ""), "file-history", JFileChooser.SAVE_DIALOG, false, null));
-        addDialogComponent(new DialogComponentBoolean(new SettingsModelBoolean(SpectraWriterNodeModel.CFGKEY_OVERWRITE, false), "Overwrite?"));
-        addDialogComponent(new DialogComponentColumnNameSelection(new SettingsModelString(SpectraWriterNodeModel.CFGKEY_COLUMN, ""), "Column to save: ", 0, SpectraValue.class));
-        addDialogComponent(new DialogComponentStringSelection(new SettingsModelString(SpectraWriterNodeModel.CFGKEY_FORMAT, "Mascot Generic Format"), "Output format", "Mascot Generic Format"));
+        addDialogComponent(new DialogComponentFileChooser(
+        		new SettingsModelString(SpectraWriterNodeModel.CFGKEY_FILE, ""), 
+        		"file-history", JFileChooser.SAVE_DIALOG, false));
+        
+        addDialogComponent(new DialogComponentBoolean(
+        		new SettingsModelBoolean(SpectraWriterNodeModel.CFGKEY_OVERWRITE, false), "Overwrite?"));
+        
+        addDialogComponent(new DialogComponentColumnNameSelection(
+        		new SettingsModelString(SpectraWriterNodeModel.CFGKEY_COLUMN, ""), "Column to save: ", 0, SpectraValue.class));
+        
+        addDialogComponent(new DialogComponentStringSelection(
+        		new SettingsModelString(SpectraWriterNodeModel.CFGKEY_FORMAT, "Mascot Generic Format"), "Output format", "Mascot Generic Format"));
+   
+        addDialogComponent(new DialogComponentColumnNameSelection (
+        		new SettingsModelString(SpectraWriterNodeModel.CFGKEY_FILENAME_SUFFIX, ""), "Suffix for save to multiple files: ", 0, false, true,
+        		new ColumnFilter() {
+
+					@Override
+					public String allFilteredMsg() {
+						return "No suitable suffix columns available (must contain no more than 40 suffixes approx.)!";
+					}
+
+					@Override
+					public boolean includeColumn(DataColumnSpec cs) {
+						if ((cs.getType().isCompatible(StringValue.class) && cs.getDomain().hasValues()) ||
+								cs.getType().isCompatible(IntValue.class)) {
+							return true;
+						}
+						return false;
+					}
+        			
+        		}));
+        	
     }
 }
 
