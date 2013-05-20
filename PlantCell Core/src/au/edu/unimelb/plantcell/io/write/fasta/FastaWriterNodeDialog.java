e2a7e19b76d2ad6baa6516fc1d61fe6673d16ee9
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-05-20 21:08:23 +0000

    Doc updates, added support for suffix for filenames (able to save to multiple files from one input table)

--- PlantCell Core/src/au/edu/unimelb/plantcell/io/write/fasta/FastaWriterNodeDialog.java
+++ PlantCell Core/src/au/edu/unimelb/plantcell/io/write/fasta/FastaWriterNodeDialog.java
@@ -2,12 +2,16 @@ package au.edu.unimelb.plantcell.io.write.fasta;
 
 import javax.swing.JFileChooser;
 
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.IntValue;
+import org.knime.core.data.StringValue;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.util.ColumnFilter;
 
 import au.edu.unimelb.plantcell.core.cells.SequenceValue;
 
@@ -25,11 +29,6 @@ import au.edu.unimelb.plantcell.core.cells.SequenceValue;
  */
 public class FastaWriterNodeDialog extends DefaultNodeSettingsPane {
 
-    /**
-     * New pane for configuring FastaWriter node dialog.
-     * This is just a suggestion to demonstrate possible default dialog
-     * components.
-     */
     @SuppressWarnings("unchecked")
 	protected FastaWriterNodeDialog() {
         super();
@@ -39,8 +38,30 @@ public class FastaWriterNodeDialog extends DefaultNodeSettingsPane {
         SettingsModelBoolean overwrite= (SettingsModelBoolean) FastaWriterNodeModel.make(FastaWriterNodeModel.CFGKEY_OVERWRITE);
         
         addDialogComponent(new DialogComponentFileChooser(filename, "file-history", JFileChooser.SAVE_DIALOG, ".fasta|.fa|.fas"));
+        
         addDialogComponent(new DialogComponentBoolean(overwrite, "Overwrite OK?"));
+        
         addDialogComponent(new DialogComponentColumnNameSelection(seq, "Sequences from... ", 0, SequenceValue.class));
+        
+        addDialogComponent(new DialogComponentColumnNameSelection (
+        		new SettingsModelString(FastaWriterNodeModel.CFGKEY_FILENAME_SUFFIX, ""), "Suffix for save to multiple files: ", 0, false, true,
+        		new ColumnFilter() {
+
+					@Override
+					public String allFilteredMsg() {
+						return "No suitable suffix columns available (must contain no more than 40 suffixes approx.)!";
+					}
+
+					@Override
+					public boolean includeColumn(DataColumnSpec cs) {
+						if ((cs.getType().isCompatible(StringValue.class) && cs.getDomain().hasValues()) ||
+								cs.getType().isCompatible(IntValue.class)) {
+							return true;
+						}
+						return false;
+					}
+        			
+        		}));
     }
 }
 
