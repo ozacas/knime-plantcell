155fa5d6b06be1b7b5910c9428ffb96b9c294c54
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-05-20 21:08:23 +0000

    Doc updates, added support for suffix for filenames (able to save to multiple files from one input table)

--- trunk/PlantCell Core/src/au/edu/unimelb/plantcell/io/write/fasta/FastaWriterNodeModel.java
+++ trunk/PlantCell Core/src/au/edu/unimelb/plantcell/io/write/fasta/FastaWriterNodeModel.java
@@ -1,10 +1,12 @@
 package au.edu.unimelb.plantcell.io.write.fasta;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataRow;
@@ -44,6 +46,7 @@ public class FastaWriterNodeModel extends NodeModel {
 	static final String CFGKEY_SEQ   = "Sequence";
 	static final String CFGKEY_FILE  = "Output Filename";
     static final String CFGKEY_OVERWRITE = "overwrite";
+	static final String CFGKEY_FILENAME_SUFFIX = "suffix";
 
     // example value: the models count variable filled from the dialog 
     // and used in the models execution method. The default components of the
@@ -52,6 +55,7 @@ public class FastaWriterNodeModel extends NodeModel {
     private final SettingsModelString       m_seq_col = (SettingsModelString) make(CFGKEY_SEQ);
     private final SettingsModelString      m_filename = (SettingsModelString) make(CFGKEY_FILE);
     private final SettingsModelBoolean    m_overwrite = (SettingsModelBoolean) make(CFGKEY_OVERWRITE);
+    private final SettingsModelString     m_suffix    = (SettingsModelString) make(CFGKEY_FILENAME_SUFFIX);
    
     /**
      * Constructor for the node model.
@@ -67,6 +71,8 @@ public class FastaWriterNodeModel extends NodeModel {
     		return new SettingsModelString(CFGKEY_FILE, ""); 
     	else if (field_name.equals(CFGKEY_OVERWRITE)) 
     		return new SettingsModelBoolean(CFGKEY_OVERWRITE, false);
+    	else if (field_name.equals(CFGKEY_FILENAME_SUFFIX)) 
+    		return new SettingsModelString(CFGKEY_FILENAME_SUFFIX, "");
     	else
     		return null;
     }
@@ -77,25 +83,33 @@ public class FastaWriterNodeModel extends NodeModel {
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
             final ExecutionContext exec) throws Exception {
 
-        logger.info("Writing fasta file... "+m_filename.getStringValue());
         String fname = m_filename.getStringValue();
         if (fname == null || fname.length() < 1) {
-        	throw new Exception("No filename specified... nothing to save!");
+        	throw new InvalidSettingsException("No filename specified... nothing to save!");
         }
         
+        // create a map from each suffix to an open file writer for saving sequences to
+        HashMap<String,PrintWriter> file_map = new HashMap<String,PrintWriter>();
+        String val = m_suffix.getStringValue();
+    	boolean use_suffix = (val != null && val.length() > 0 && !val.equalsIgnoreCase("<none>"));
+    	File basename = new File(fname);
+    	int suffix_idx = -1;
+    	if (!use_suffix) {
+    		file_map.put(basename.getName(), new PrintWriter(new FileWriter(basename)));
+    	} else {
+    		suffix_idx = inData[0].getSpec().findColumnIndex(m_suffix.getStringValue());
+    		if (suffix_idx < 0)
+    			throw new InvalidSettingsException("Cannot locate suffix column... aborting! "+m_suffix.getStringValue());
+    	}
+    	
+    	// which column has the sequences?
         int seq_idx = inData[0].getSpec().findColumnIndex(m_seq_col.getStringValue());
         if (seq_idx  < 0) 
-        	throw new InvalidSettingsException("");
+        	throw new InvalidSettingsException("Cannot find sequence column: "+m_seq_col.getStringValue());
         
-        File f = new File(fname);
-        if (!m_overwrite.getBooleanValue() && f.exists()) {
-        	throw new Exception("Will not overwrite existing: "+fname+" - configure the node to override if this is what you want.");
-        }
         // replicate input data on output port
         DataTableSpec inSpec  = inData[0].getDataTableSpec();
        
-        PrintWriter out_stream = new PrintWriter(new BufferedWriter(new FileWriter(fname)));
-      
         ColumnRearranger outputTable = new ColumnRearranger(inSpec);
         int[] cols = new int[inSpec.getNumColumns()];
         for (int i=0; i<cols.length; i++) {
@@ -110,22 +124,87 @@ public class FastaWriterNodeModel extends NodeModel {
         	if (sc == null || sc.isMissing())
         		continue;
         	SequenceValue sv = (SequenceValue) sc;
-        	
+        	String id = sv.getID();
+    		id = id.replaceAll("[\\r\\n]+", " ");
+    		
+    		// never fails (unless exception thrown) and the writer is ready for use...
+    		PrintWriter pw = get_pw(use_suffix, suffix_idx, r, file_map, basename);
+    		
         	if (sv.hasDescription()) {
-        		out_stream.print(">"+sv.getID());
-        		out_stream.print(" ");
-        		out_stream.println(sv.getDescription());
+        		String descr = sv.getDescription();			
+        		descr = descr.replaceAll("[\\r\\n]+", " "); // ensure no line breaks in the description cause problems
+        		pw.print(">"+id);
+        		pw.print(" ");
+        		pw.println(descr);
         	} else {
-        		out_stream.println(">"+sv.getID());
+        		pw.println(">"+id);
         	}
-        	out_stream.println(sv.getStringValue());
+        	pw.println(sv.getStringValue());
         }
-        out_stream.close();
+        
+        // close all files from the file map 
+        logger.info("Saved sequences to "+file_map.size()+" files.");
+        for (String s : file_map.keySet()) {
+        	PrintWriter pw = file_map.get(s);
+        	pw.close();
+        }
+        
         BufferedDataTable out = exec.createColumnRearrangeTable(inData[0], outputTable, exec);
         // once we are done, we close the container and return its table
         return new BufferedDataTable[] { out };
     }
 
+    /**
+     * HACK TODO: code copied from SpectraWriterNodeModel... need a manager class for this!
+     * 
+     * Create a print writer which is ready for use based on node configuration and the current row (containing the peak list to be saved).
+     * The map is updated to include the PrintWriter for further use if needed
+     * 
+     * @param use_suffix
+     * @param suffix_idx
+     * @param r
+     * @param file_map
+     * @return
+     * @throws IOException
+     * @throws InvalidSettingsException if an invalid suffix is encountered during execute() or if a file already exists and the node configuration does not permit overwrite
+     */
+    private PrintWriter get_pw(boolean use_suffix, int suffix_idx, DataRow r,
+			HashMap<String, PrintWriter> file_map, File basename) throws InvalidSettingsException, IOException {
+		if (!use_suffix) {
+			return file_map.get(basename.getName());
+		} else {
+			String bn = basename.getName();
+			Pattern p = Pattern.compile("^(.*)(\\.[a-zA-Z0-9]+)$");
+			Matcher m = p.matcher(bn);
+			DataCell suffix_cell =  r.getCell(suffix_idx);
+			if (suffix_cell == null || suffix_cell.isMissing())
+				throw new InvalidSettingsException("Suffix cannot be missing!");
+			String suffix_to_use = suffix_cell.toString();
+			if (suffix_to_use.indexOf(File.separatorChar) >= 0)
+				throw new InvalidSettingsException("Suffix cannot contain: "+File.separatorChar);
+			
+			File f = null;
+			if (m.matches()) {
+				String sans_ext = m.group(1);
+				f = new File(basename.getParent(), sans_ext+suffix_cell.toString()+m.group(2));
+			} else {
+				f = new File(basename.getParent(), bn+suffix_cell.toString());
+			}
+			
+			if (file_map.containsKey(f.getName())) {
+				return file_map.get(f.getName());
+			} else {
+				if (f.exists() && !m_overwrite.getBooleanValue())
+					throw new InvalidSettingsException("Will not overwrite existing: "+f.getAbsolutePath());
+				
+				logger.info("Saving sequences to "+f.getAbsolutePath());
+				PrintWriter pw = new PrintWriter(new FileWriter(f));
+				file_map.put(f.getName(), pw);
+				return pw;
+			}
+		}
+	}
+    
     /**
      * {@inheritDoc}
      */
@@ -151,6 +230,7 @@ public class FastaWriterNodeModel extends NodeModel {
          m_seq_col.saveSettingsTo(settings);
          m_filename.saveSettingsTo(settings); 
          m_overwrite.saveSettingsTo(settings);
+         m_suffix.saveSettingsTo(settings);
     }
 
     /**
@@ -162,6 +242,12 @@ public class FastaWriterNodeModel extends NodeModel {
         m_seq_col.loadSettingsFrom(settings);
         m_filename.loadSettingsFrom(settings); 
         m_overwrite.loadSettingsFrom(settings);
+        // backward compatibility with existing workflows
+        if (settings.containsKey(CFGKEY_FILENAME_SUFFIX)) {
+        	m_suffix.loadSettingsFrom(settings);
+        } else {
+        	m_suffix.setStringValue("");
+        }
     }
 
     /**
@@ -173,6 +259,10 @@ public class FastaWriterNodeModel extends NodeModel {
          m_seq_col.validateSettings(settings);
          m_filename.validateSettings(settings);
          m_overwrite.validateSettings(settings);
+         // backward compatibility with existing workflows
+         if (settings.containsKey(CFGKEY_FILENAME_SUFFIX)) {
+         	m_suffix.validateSettings(settings);
+         } 
     }
     
     /**
