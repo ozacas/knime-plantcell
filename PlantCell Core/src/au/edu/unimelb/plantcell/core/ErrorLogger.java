1392cc951813aef8d3b5eb396ea203f077987347
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-06-03 21:50:23 +0000

    Added an alternate constructor which can log to the warning level unless keywords are found in the message suggestive that an error level is more appropriate. All messages are logged, only the level is different.

--- PlantCell Core/src/au/edu/unimelb/plantcell/core/ErrorLogger.java
+++ PlantCell Core/src/au/edu/unimelb/plantcell/core/ErrorLogger.java
@@ -12,15 +12,41 @@ import org.knime.core.node.NodeLogger;
  */
 public class ErrorLogger extends LogOutputStream {
 	private NodeLogger m_l;
+	private boolean m_keywords;
 	
+	/**
+	 * Default constructor. All messages logged at an error level
+	 * @param l
+	 */
 	public ErrorLogger(NodeLogger l) {
 		super();
+		m_l = l;
+		m_keywords = false;
+	}
+	
+	/**
+	 * 
+	 * @param l
+	 * @param require_keywords iff true only those log messages with error|problem|correct|fix|... will be logged at an error level, all else warning level
+	 */
+	public ErrorLogger(NodeLogger l, boolean require_keywords) {
+		m_keywords = require_keywords;
 		m_l = l;
 	}
 	
 	@Override
 	protected void processLine(String arg0, int arg1) {
-		m_l.error(arg0);
+		if (!m_keywords) {
+			m_l.error(arg0);
+		} else {
+			String str = arg0.toLowerCase();
+			if (str.indexOf("error") >= 0 || str.indexOf("problem") >= 0 || 
+					str.indexOf("wrong") >= 0|| str.indexOf("problem") >= 0 || str.indexOf("fix") >= 0) {
+				m_l.error(arg0);
+			} else {
+				m_l.warn(arg0);
+			}
+		}
 	}
 
 }
