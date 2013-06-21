431b7b29e787abd3d11699d904e128dd83a8afc6
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-06-21 00:54:38 +0000

    Commit superclass-compatible code for now.

--- Views/src/au/edu/unimelb/plantcell/views/bar3d/MyCameraMouseController.java
+++ Views/src/au/edu/unimelb/plantcell/views/bar3d/MyCameraMouseController.java
@@ -7,25 +7,27 @@ import org.jzy3d.chart.controllers.mouse.camera.CameraMouseController;
 import org.jzy3d.maths.Coord3d;
 
 
-
-
 public class MyCameraMouseController extends CameraMouseController {
+	@SuppressWarnings("unused")
 	private float cumulative = 0.0f;
+	@SuppressWarnings("unused")
 	private Coord3d original_viewpoint = null;
 	
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent ev) {
-		float rot = (float) ev.getPreciseWheelRotation();
+		super.mouseWheelMoved(ev);
+		/*float rot = (float) ev.getPreciseWheelRotation();
 		
 		cumulative += rot;
 	
 		Logger.getAnonymousLogger().info("Cumulative rotation: "+cumulative);
 		
+		@SuppressWarnings("unused")
 		float factor = 1.0f;
 		if (cumulative < 0.0f) 
 			factor = 1.0f - (cumulative / 100.0f);
 		else if (cumulative > 0.0f)
-			factor = 1.0f + (cumulative / 100.0f);
+			factor = 1.0f + (cumulative / 100.0f);*/
 		
 	}
 }
