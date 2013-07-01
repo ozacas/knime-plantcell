c7d6ccbc0fa402dd3b57b84d6e131a83d17986ea
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-07-01 21:04:34 +0000

    Display the numeric range of each variable in axis ticks rather than [0,1]

---
+++ trunk/Views/src/au/edu/unimelb/plantcell/views/bar3d/MyAxisRenderer.java
@@ -0,0 +1,27 @@
+package au.edu.unimelb.plantcell.views.bar3d;
+
+import org.jzy3d.plot3d.primitives.axes.layout.renderers.ITickRenderer;
+
+/**
+ * A simple-minded numeric renderer which displays the axis values rather than the computational range of the chart
+ * 
+ * @author andrew.cassin
+ *
+ */
+public class MyAxisRenderer implements ITickRenderer {
+	private float min;
+	private float max;
+	
+	public MyAxisRenderer(double x_min, double x_max) {
+		assert((float) x_max >=  x_min);
+		
+		this.min = (float) x_min;
+		this.max = (float) x_max;
+	}
+	
+	@Override
+	public String format(float value) {
+		return String.valueOf(min + value * (max-min));
+	}
+
+}
