50d3e751a3e29708fc29de8533b61221e52ae803
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2013-09-12 02:29:54 +0000

    fixed bug with JComboBox support: old value would not be initialized correctly during configure()

--- trunk/Emboss (local)/src/au/edu/unimelb/plantcell/io/jemboss/simple/StandardEmbossDialog.java
+++ trunk/Emboss (local)/src/au/edu/unimelb/plantcell/io/jemboss/simple/StandardEmbossDialog.java
@@ -3,7 +3,9 @@ package au.edu.unimelb.plantcell.io.jemboss.simple;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -11,18 +13,26 @@ import javax.swing.JEditorPane;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.ListSelectionModel;
 
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentString;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
+/**
+ * 
+ * @author andrew.cassin
+ *
+ */
 public class StandardEmbossDialog extends DefaultNodeSettingsPane {
 		private final JEditorPane           m_html_help = new JEditorPane();
 		private ACDApplication              m_cur_prog  = null;		// currently selected program
 		final   UserSettingsPanel   user_settings_panel = new UserSettingsPanel();
-		final   JList                    list_of_fields = new JList(new String[] { "field1" });
+		final   JList<String>            list_of_fields = new JList<String>(new String[] { "field1" });
 
 		public StandardEmbossDialog() {
 			super();
@@ -55,30 +65,52 @@ public class StandardEmbossDialog extends DefaultNodeSettingsPane {
 		}
 		
 		public void addAdvancedTab() {	
+		    addDialogComponent(new DialogComponentString(
+        			new SettingsModelString(EmbossPredictorNodeModel.CFGKEY_ARGS, ""), "Arguments (advanced users only)"
+            ));
+	        
+			  
 			final JPanel adv_params_tab = new JPanel();
 	        adv_params_tab.setLayout(new BoxLayout(adv_params_tab, BoxLayout.Y_AXIS));
 	        final JPanel top_panel = new JPanel();
 	        top_panel.setLayout(new BoxLayout(top_panel, BoxLayout.X_AXIS));
+	        
 	        JPanel fields_panel = new JPanel();
 	        fields_panel.setLayout(new BoxLayout(fields_panel, BoxLayout.Y_AXIS));
+	        fields_panel.setBorder(BorderFactory.createTitledBorder("Custom parameters"));
+	        
 	        user_settings_panel.setLayout(new BoxLayout(user_settings_panel, BoxLayout.Y_AXIS));
 	        top_panel.add(fields_panel);
 	        top_panel.add(Box.createRigidArea(new Dimension(5,0)));
+	        JPanel button_panel = new JPanel();
+	        button_panel.setLayout(new BoxLayout(button_panel, BoxLayout.Y_AXIS));
+	        
+	        top_panel.add(button_panel);
+	        top_panel.add(Box.createRigidArea(new Dimension(5,0)));
 	        JPanel p = new JPanel();
+	        JTabbedPane tab_pane = new JTabbedPane();
+	        tab_pane.addTab("Custom parameter settings", null, new JScrollPane(user_settings_panel), "Lets you control exact behaviour of chosen program");
 	        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-	        p.add(new JScrollPane(user_settings_panel));
-	        top_panel.add(p);
+	        JScrollPane sp = new JScrollPane(m_html_help);
+	        tab_pane.addTab("Help about custom parameters", null, sp, "");
+	        top_panel.add(tab_pane);
+	        list_of_fields.setMinimumSize(new Dimension(200,200));
 	        fields_panel.add(new JScrollPane(list_of_fields));
-	        JButton b_add = new JButton("Add");
-	        JButton b_remove = new JButton("Remove");
+	        
+	        JButton b_add = new JButton("Add >>");
+	        JButton b_remove = new JButton("<< Remove");
+	        button_panel.add(b_add);
+	        button_panel.add(Box.createRigidArea(new Dimension(20,20)));
+	        button_panel.add(b_remove);
+	        
 	        b_add.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					if (m_cur_prog == null)
 						return;
-					Object[] selected = list_of_fields.getSelectedValues();
-					if (selected == null || selected.length < 1)
+					List<String> selected = list_of_fields.getSelectedValuesList();
+					if (selected == null || selected.size() < 1)
 						return;
 					for (Object sel : selected) {
 						ACDField f = m_cur_prog.getField(sel.toString());
@@ -100,8 +132,8 @@ public class StandardEmbossDialog extends DefaultNodeSettingsPane {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					Object[] o = list_of_fields.getSelectedValues();
-					if (o == null || o.length < 1)
+					List<String> o = list_of_fields.getSelectedValuesList();
+					if (o == null || o.size() < 1)
 						return;
 					for (Object sel : o) {
 						String cur_field = sel.toString();
@@ -114,17 +146,11 @@ public class StandardEmbossDialog extends DefaultNodeSettingsPane {
 				}
 	        	
 	        });
-	        fields_panel.add(b_add);
-	        fields_panel.add(b_remove);
-	        JScrollPane sp = new JScrollPane(m_html_help);
-	        sp.setMinimumSize(new Dimension(300,120));
-	        sp.setPreferredSize(new Dimension(400,130));
-	        sp.setMaximumSize(new Dimension(600,400));
-	        JPanel bottom_panel = new JPanel();
-	        bottom_panel.setLayout(new BoxLayout(bottom_panel, BoxLayout.X_AXIS));
-	        bottom_panel.add(sp);
+	        
+	      
+	       
+	    
 	        adv_params_tab.add(top_panel);
-	        adv_params_tab.add(bottom_panel);
 	        
 	        addTab("Advanced", adv_params_tab);
 		}
