90adbb91972370291c1020496c28355ba6e63ae8
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2014-03-18 06:32:51 +0000

    Added additional function to permit flexibility required by iTRAQ node (see Proteomics for more details)

--- trunk/PlantCell Core/src/au/edu/unimelb/plantcell/core/ExternalProgram.java
+++ trunk/PlantCell Core/src/au/edu/unimelb/plantcell/core/ExternalProgram.java
@@ -12,12 +12,9 @@ public class ExternalProgram {
 	public ExternalProgram() {
 	}
 	
-	public static File find(String s_folder, String progname) {
-		String[] prognames = new String[] { progname, progname + ".exe" };
-		File folder = new File(s_folder);
-		File[] folders = new File[] { folder, new File(folder, "bin") };
-		for (File f : folders) {
-			for (String p : prognames) {
+	public static File find(final String[] filenames, final File[] folder_names) {
+		for (File f : folder_names) {
+			for (String p : filenames) {
 				File tmp = new File(f, p);
 				if (tmp.canExecute() && tmp.isFile()) {
 					return tmp;
@@ -26,5 +23,20 @@ public class ExternalProgram {
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Tries various platform-specific tricks to find the specified executable <code>progname</code>
+	 * in the specified folder <code>s_folder</code>
+	 * 
+	 * @param s_folder
+	 * @param progname
+	 * @return null if nothing is found which is executable and accessible
+	 */
+	public static File find(String s_folder, String progname) {
+		String[] prognames = new String[] { progname, progname + ".exe" };
+		File folder = new File(s_folder);
+		File[] folders = new File[] { folder, new File(folder, "bin") };
+		return find(prognames, folders);
 	}
 }
