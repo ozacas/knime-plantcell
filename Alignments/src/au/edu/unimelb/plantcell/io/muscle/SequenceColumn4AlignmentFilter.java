f4dc06521cd6dc425d4fe37ccb1e9ce19440dbdc
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2014-05-09 05:26:29 +0000

    Bumped alignment plugin version and added javadoc for SequenceColumn4AlignmentFilter class

--- Alignments/src/au/edu/unimelb/plantcell/io/muscle/SequenceColumn4AlignmentFilter.java
+++ Alignments/src/au/edu/unimelb/plantcell/io/muscle/SequenceColumn4AlignmentFilter.java
@@ -5,6 +5,15 @@ import org.knime.core.node.util.ColumnFilter;
 
 import au.edu.unimelb.plantcell.core.cells.SequenceValue;
 
+/**
+ * Shared by all the alignment nodes to accept columns under one of two conditions:
+ * 1) the column contains SequenceValue instance's. All the valid sequences are pooled and then aligned as one.
+ * 2) the column contains a collection of SequenceValue instances (list or set). In this case the alignments
+ *    are done separately per collection
+ *    
+ * @author http://www.plantcell.unimelb.edu.au/bioinformatics
+ *
+ */
 public class SequenceColumn4AlignmentFilter implements ColumnFilter {
 
 	@Override
@@ -14,6 +23,9 @@ public class SequenceColumn4AlignmentFilter implements ColumnFilter {
 
 	@Override
 	public boolean includeColumn(DataColumnSpec arg0) {
+		if (arg0 == null || arg0.getType() == null)
+			return false;
+		
 		// collection cell?
 		if (arg0.getType().isCollectionType() && arg0.getType().getCollectionElementType().isCompatible(SequenceValue.class))
 			return true;
