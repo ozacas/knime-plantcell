7c8d482eb5c2ea88d4af0975dfd6f359f98605b6
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@dddfb942-a9a2-2c26-f143-85623fb4cac2>
2014-06-03 05:59:41 +0000

    beginnings of node to support calling mascot with input parameters from a table

---
+++ Proteomics/src/au/edu/unimelb/plantcell/io/ws/mascot/msms/AdvMSMSSearchNodeDialog.java
@@ -0,0 +1,177 @@
+package au.edu.unimelb.plantcell.io.ws.mascot.msms;
+
+import javax.swing.JFileChooser;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.StringValue;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentButtonGroup;
+import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
+import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.DialogComponentNumberEdit;
+import org.knime.core.node.defaultnodesettings.DialogComponentPasswordField;
+import org.knime.core.node.defaultnodesettings.DialogComponentString;
+import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelDoubleBounded;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.util.ColumnFilter;
+
+import au.edu.unimelb.plantcell.io.read.mascot.MascotReaderNodeModel;
+
+
+/**
+ * <code>NodeDialog</code> for the "AdvMSMSSearch" Node.
+ * Permits search and download of MS/MS Ion Searches using a MascotEE interface.
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more 
+ * complex dialog please derive directly from 
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author http://www.plantcell.unimelb.edu.au/bioinformatics
+ */
+public class AdvMSMSSearchNodeDialog extends DefaultNodeSettingsPane {
+	private final SettingsModelString m_url = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_MASCOTEE_URL, "");
+	private final SettingsModelDoubleBounded ci = new SettingsModelDoubleBounded(MascotReaderNodeModel.CFGKEY_CONFIDENCE, 
+			MascotReaderNodeModel.DEFAULT_CONFIDENCE, 0.0, 1.0);
+	private final SettingsModelString m_database          = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_DATABASE, "");
+	private final SettingsModelString m_enzyme            = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_ENZYME, "");
+	private final SettingsModelString m_fixed_mods        = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_FIXED_MODS, "");
+	private final SettingsModelString m_variable_mods     = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_VARIABLE_MODS, "");
+	private final SettingsModelString m_mass_type         = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_MASS_TYPE, "");
+	private final SettingsModelString m_instrument        = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_INSTRUMENT, "");
+	private final SettingsModelString m_taxon             = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_TAXONOMY, "");
+	private final SettingsModelString m_top_hits          = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_REPORT_TOP, "");
+	private final SettingsModelString m_peptide_charge    = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_PEPTIDE_CHARGE, "");
+	private final SettingsModelString m_allowed_prot_mass = new SettingsModelString(AdvMSMSSearchNodeModel.CFGKEY_ALLOWED_PROTEIN_MASS, "");
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public AdvMSMSSearchNodeDialog() {
+		createNewGroup("MascotEE Server URL");
+		this.setHorizontalPlacement(true);
+		addDialogComponent(new DialogComponentString(m_url, "MascotEE URL", true, 60));
+		this.setHorizontalPlacement(false);
+		
+    	addDialogComponent(new DialogComponentString(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_MASCOTEE_USER, ""), "Username"));
+    	addDialogComponent(new DialogComponentPasswordField(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_MASCOTEE_PASSWD, ""), "Password"));
+    	
+		createNewGroup("Store mascot result files into...");
+		addDialogComponent(new DialogComponentFileChooser(
+				new SettingsModelString(MSMSSearchNodeModel.CFGKEY_OUT_DAT, ""), "mascot-results-folder", JFileChooser.OPEN_DIALOG, true));
+		
+		createNewTab("Mascot Settings");
+		addDialogComponent(new DialogComponentColumnNameSelection(m_database,   "Search Database", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_enzyme,     "Enzyme", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_fixed_mods, "Fixed Modifications", 0, false, true, new ColumnFilter() {
+
+			@Override
+			public boolean includeColumn(DataColumnSpec colSpec) {
+				return (colSpec.getType().isCollectionType() && 
+						colSpec.getType().getCollectionElementType().isCompatible(StringValue.class));
+			}
+
+			@Override
+			public String allFilteredMsg() {
+				return "Fixed modifications must be a list (eg. groupby list) of String cells";
+			}
+			
+		}));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_variable_mods, "Variable Modifications", 0, false, true, new ColumnFilter() {
+
+			@Override
+			public boolean includeColumn(DataColumnSpec colSpec) {
+				return (colSpec.getType().isCollectionType() && 
+						colSpec.getType().getCollectionElementType().isCompatible(StringValue.class));
+			}
+
+			@Override
+			public String allFilteredMsg() {
+				return "Variable modifications must be a list (eg. groupby list) of String cells";
+			}
+			
+		}));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_instrument, "Instrument", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_mass_type, "Mass Type", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_allowed_prot_mass, "Allowed protein mass", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_peptide_charge, "Peptide charge", 0, StringValue.class));
+		addDialogComponent(new DialogComponentColumnNameSelection(m_taxon, "Taxonomy", 0, StringValue.class));
+		
+		/*addDialogComponent(dc_report_overview);
+		addDialogComponent(dc_report_top);
+		addDialogComponent(dc_icat_quant);
+		addDialogComponent(dc_precursor);
+		addDialogComponent(dc_username);
+		addDialogComponent(dc_email);
+		addDialogComponent(dc_job_title);*/
+	
+		createNewTab("Tolerance");
+		createNewGroup("Peptide Tolerance");
+		this.setHorizontalPlacement(true);
+		addDialogComponent(new DialogComponentString(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_PEPTIDE_TOL_VALUE, ""), ""));
+		String[] peptide_units = new String[] { "Da", "mmu", "%", "ppm" };
+		addDialogComponent(new DialogComponentStringSelection(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_PEPTIDE_TOL_UNITS, ""), "", peptide_units));
+		this.setHorizontalPlacement(false);
+		
+		createNewGroup("MS/MS Tolerance");
+		this.setHorizontalPlacement(true);
+		addDialogComponent(new DialogComponentString(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_MSMS_TOL_VALUE, ""), ""));
+		String[] msms_units = new String[] { "Da", "mmu" };
+		addDialogComponent(new DialogComponentStringSelection(new SettingsModelString(MSMSSearchNodeModel.CFGKEY_MSMS_TOL_UNITS, ""), "", msms_units));
+		this.setHorizontalPlacement(false);
+	
+		createNewTab("Results Processing");
+    	final SettingsModelString result_type = new SettingsModelString(MascotReaderNodeModel.CFGKEY_RESULTTYPE, MascotReaderNodeModel.DEFAULT_RESULTTYPE);
+    	DialogComponentButtonGroup bg = new DialogComponentButtonGroup(result_type, true, "Report which peptide hits per query?", 
+    			MascotReaderNodeModel.RESULT_TYPES);
+    	result_type.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				updateConfidenceField(result_type.getStringValue());
+			}
+    		
+    	});
+        bg.setToolTipText("Which peptide identifications per spectra do you want to see?");
+        addDialogComponent(bg);
+        addDialogComponent(new DialogComponentNumberEdit(ci,"Identity Threshold Confidence", 5));
+        addDialogComponent(new DialogComponentBoolean(new SettingsModelBoolean(MascotReaderNodeModel.CFGKEY_WANT_SPECTRA, true), "Want MS/MS spectra?"));
+	
+	}
+	
+	protected void updateConfidenceField(String sv) {
+		if (sv == null) {
+			sv = MascotReaderNodeModel.DEFAULT_RESULTTYPE;
+		}
+		ci.setEnabled(sv.trim().toLowerCase().startsWith("confident"));
+	}
+	
+	@Override
+	public void loadAdditionalSettingsFrom(final NodeSettingsRO settings, final DataTableSpec[] inSpec) {
+		try {
+			m_url.setStringValue(settings.getString(MSMSSearchNodeModel.CFGKEY_MASCOTEE_URL));
+
+			// look at configured result type and decide what widgets to enable
+			String result_type = settings.getString(MascotReaderNodeModel.CFGKEY_RESULTTYPE);
+			updateConfidenceField(result_type);
+		} catch (InvalidSettingsException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+    public void saveAdditionalSettingsTo(NodeSettingsWO settings) {
+    	// since this node doesnt support files but the superclass of the nodemodel implementation does, we
+    	// must fake dialog values for those settings...
+    	settings.addStringArray(MascotReaderNodeModel.CFGKEY_FILES, new String[0]);
+    }
+}
